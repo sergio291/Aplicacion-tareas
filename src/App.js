6f7bde014470487fb6d0e2f189a79ef54bc62b22
sergio291 <128194437+sergio291@users.noreply.github.com>
2024-05-02 20:09:43 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo'
+          alt='logo de freecodecamp' />
+      </div>
+      <div className='tarea-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
